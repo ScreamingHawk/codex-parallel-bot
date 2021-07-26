3f6ae0bbb8948d3f233db9798f6f5d9dadf6749d
Michael Standen <ScreamingHawk@gmail.com>
2021-07-27 10:54:00 +1200

    Fix price calculation for non eth denom tokens

--- src/util/format.js
+++ src/util/format.js
@@ -1,7 +1,12 @@
 const { ethers } = require('ethers')
 
+const { formatEther } = ethers.utils
+
+const formatDollar = num => `$${Number.parseFloat(num).toFixed(2)}`
+
 module.exports = {
 	formatNumber: num => Number.parseFloat(num).toPrecision(4),
-	formatEther: ethers.utils.formatEther, // Expects BigNumber
-	formatDollar: num => `$${Number.parseFloat(num).toFixed(2)}`,
+	formatEther, // Expects BigNumber
+	formatEtherDollar: num => formatDollar(formatEther(num)), // Expects BigNumber
+	formatDollar,
 }
